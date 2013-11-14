599adce1fa60044b517bc59139941ec3a86b36d9
Theo Högberg <thogberg@atlassian.com>
2013-11-13 17:00:42 -0800

    init

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+
+/*
+ * GET home page.
+ */
+
+ exports.index = function(req, res){
+ 	res.render('index', { title: 'Express', 
+ 		dulce: 'Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori. Dulce et decorum est pro patria mori.'
+ 	});
+};
