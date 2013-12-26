24dc947f1308741f81fd2cd4e3993d6451800c65
theo <toshi@theo-tosh.(none)>
2013-12-26 21:54:28 +0100

    removed pg module

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,6 @@
  	res.render('index', { 
  		header: 'My hackerspace', 
  		ingress: 'This is my site, here I do stuff with computers.',
- 		content: 'This site is running on node.js, express framework, the styling is made with a custom twitter bootstrap template. This webapp was designed and created by Theo Högberg. Dolce et decorum est pro patria mori'
+ 		content: 'This site is running on node.js, express framework, the styling is made with a custom twitter bootstrap template.'
  	});
 };
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,6 @@
  * GET users listing.
  */
 
-exports.list = function(req, res){
-  res.send("respond with a resource, the source is nothing");
+exports.user = function(req, res){
+  res.send("respond with a resource, the source is DONKEY KONKEY");
 };
