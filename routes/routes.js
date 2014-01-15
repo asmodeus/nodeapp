624b51898b5e1d8a88ab3ad918c7f6147a1319ef
theo <toshi@theo-tosh.(none)>
2014-01-15 16:51:20 +0100

    Refactored alot of code, discovered callbacks

--- routes/routes.js
+++ routes/routes.js
@@ -1,33 +1,32 @@
 /*
  * GET home page.
  */
-  var db = require('./db');
+ var _ = require('underscore');
+ var db = require('../db');
+ db.createConnection();
 
  exports.about = function(req, res){
- 	res.render('index', { 
- 		title: '', 
- 		ingress: 'Welcome. I am a techie originating from Stockholm, Sweden.',
- 		content: 'My name is Theo, here I display projects I code away at during rainy, snowy and sunny days.This site is running on heroku using node.js, express framework, ejs and postgres. The styling is made with a custom twitter bootstrap template. The virtualized heroku client is itself running on the amazon cloud (AWS). Feel free to mail me at <link>theoswe@gmail.com<link> if you are interested in hiring me for something or if you just got a question to ask.'
+ 	db.dbCall("select * from posts where category = 'about' order by post_date", function(data){
+ 		res.render('index', { viewdata: data});
  	});
  };
- 
+
+ exports.processing = function(req, res){
+ 	db.dbCall("select * from posts where category = 'projects' order by post_date", function(data){
+ 		res.render('index', { viewdata: data});
+ 	});
+ }; 
+
+ exports.blog = function(req, res){
+ 	db.dbCall("select * from posts where category = 'blog' order by post_date", function(data){
+ 		res.render('index', { viewdata: data});
+ 	});
+ }; 
 
  exports.home = function(req, res){
- 	res.render('index', { 
- 		title: '', 
- 		ingress: '',
- 		content: ''
- 	});
- }; 
-
- exports.ath = function(req, res){
- 	res.render('index', { 
+ 	res.render('home', { 
  		title: 'some random cool stuff', 
  		ingress: 'Furry feet',
- 		content: 'What do being a furry adventurer and telecommuting have in common?'
+ 		content: 'What do being a hobbit and telecommuting have in common?'
  	});
  }; 
-
- exports.test = function(req, res){
- 	res.send(db.titles());
- };
