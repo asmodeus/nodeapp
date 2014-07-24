fc167331f48abe5425837fc697a275b0d37cff81
asmodeus <theoswe@gmail.com>
2014-07-24 13:07:22 +0200

    Added some processing examples

--- createposttable.sql
+++ createposttable.sql
@@ -13,16 +13,17 @@ CREATE TABLE posts (
 INSERT INTO posts ( post_no, ingress, text_content, category ) 
 VALUES (
 1,
-'Welcome into the darkness.',
-'Here I display projects I''ve done like this site for example. This site is running on heroku using node.js, express framework, ejs and postgres. The styling is made with a custom twitter bootstrap template. More info on heroku here. Feel free to mail me at <link>theoswe@gmail.com<link> if you are interested in hiring me, or perhaps you just got a question about my work',
-'about' );
+'Risus',
+'"Only a Stradivarius can be used to seduce a Vampire Prince"',
+'rpg' );
 INSERT INTO posts ( post_no, title, ingress, text_content, category ) 
 VALUES (
 2, 
-'i am not dead yet',
-'This is my story.', 
-'...', 
-'about');
+'',
+'Risus', 
+'"While wielding the Sword of Mercy, you always roll dice at least equal to
+your foe, but you must spare his life if you win."', 
+'rpg');
 
 
 
